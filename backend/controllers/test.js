3b58adabc8a05ea74f38a0ebc6d6afc741e8e2bc
Grey Johnson <grey.johnson@ufl.edu>
2021-02-26 12:28:58 -0500

    Renamed function

--- backend/controllers/test.js
+++ backend/controllers/test.js
@@ -1,19 +1,19 @@
 const express = require('express');
 
-function fillAsHtml(response, text) {
+function sendAsHtml(response, text) {
     response.status(200);
     response.set('Content-Type', 'text/html');
     response.send(`<html><body><p>${text}</p></body></html>`);
 }
 
 exports.base = function(request, response) {
-    fillAsHtml(response, 'This is the test home page.');
+    sendAsHtml(response, 'This is the test home page.');
 }
 
 exports.student = function(request, response) {
-    fillAsHtml(response, 'Hello, Student.');
+    sendAsHtml(response, 'Hello, Student.');
 }
 
 exports.admin = function(request, response) {
-    fillAsHtml(response, 'Hello, Admin.');
+    sendAsHtml(response, 'Hello, Admin.');
 }
