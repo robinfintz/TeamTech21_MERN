851b434d6ff8dd137cfa4cc777c974f0b8d1d66d
vidatonkova <vidatonkova@ufl.edu>
2021-03-08 20:07:37 -0500

    nav bar + lil vscode demo

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -16,9 +16,8 @@ const Home = () => {
               <div className="spacer" > &nbsp; </div>
               <div className="spacer" > &nbsp; </div>
               <Row>
-                  <Col>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus consequat metus vitae euismod. Proin pretium, libero in accumsan pellentesque, neque nisl ullamcorper felis, at condimentum metus massa eu magna. Donec id dapibus tellus, in sodales eros. Curabitur a diam ligula. Mauris ullamcorper gravida scelerisque. Ut cursus gravida libero id tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer egestas congue dui vitae sollicitudin. Ut scelerisque dapibus sapien, et varius purus interdum a.</Col>
-                  <Col>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus consequat metus vitae euismod. Proin pretium, libero in accumsan pellentesque, neque nisl ullamcorper felis, at condimentum metus massa eu magna. Donec id dapibus tellus, in sodales eros. Curabitur a diam ligula. Mauris ullamcorper gravida scelerisque. Ut cursus gravida libero id tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer egestas congue dui vitae sollicitudin. Ut scelerisque dapibus sapien, et varius purus interdum a.</Col>
-                  <Col>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus consequat metus vitae euismod. Proin pretium, libero in accumsan pellentesque, neque nisl ullamcorper felis, at condimentum metus massa eu magna. Donec id dapibus tellus, in sodales eros. Curabitur a diam ligula. Mauris ullamcorper gravida scelerisque. Ut cursus gravida libero id tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer egestas congue dui vitae sollicitudin. Ut scelerisque dapibus sapien, et varius purus interdum a.</Col>
+                  <Col>hello bb</Col>
+                  <Col>its me, mario</Col>
               </Row>
             </Container>
         </div>
