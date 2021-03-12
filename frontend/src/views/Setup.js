915d848356bc9ba1c80c2993224ab8db442cce69
Yanna Lin <yannalin@MacBook-Pro.local>
2021-03-11 20:54:34 -0500

    started some sort of table?

--- frontend/src/views/Setup.js
+++ frontend/src/views/Setup.js
@@ -21,7 +21,19 @@ const Setup = () => {
               <div className="spacer" > &nbsp; </div>
               <div className="spacer" > &nbsp; </div>
               <div className="spacer" > &nbsp; </div>
-              <Row>
+
+              <table>
+                  <tr>
+                      <th className={{fontSize:"300%"}}>Sensor Type</th>
+                      <th>ID#/Name</th>
+                      <th>Location/Status</th>
+                      <th>Other Details</th>
+                      
+                  </tr>
+              </table>
+
+
+              {/* <Row>
                   <Col>According to all known laws of aviation, there is no way a bee should be able to fly.</Col>
                   <Col>Its wings are too small to get its fat little body off the ground.</Col>
                   <Col>The bee, of course, flies anyway because bees don't care what humans think is impossible.</Col>
@@ -30,7 +42,7 @@ const Setup = () => {
 
               <Row>
                     <a href = "https://www.youtube.com/watch?v=L_vnEHDjfZ8">CLICK ME--NOT A RICK ROLL!!</a>
-              </Row>
+              </Row> */}
             </Container>
         </div>
         );
