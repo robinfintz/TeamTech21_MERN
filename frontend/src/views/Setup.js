98ff43c2012c12ecba2f48ee0cb084d8c6ce0054
Yanna Lin <yannalin@MacBook-Pro.local>
2021-03-18 16:04:40 -0400

    bg beige rectangle added

--- frontend/src/views/Setup.js
+++ frontend/src/views/Setup.js
@@ -6,46 +6,87 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Setup = () => {
-        return (
-        <div>
-            <Container>
-              <Row>
-                  <Col style={{textAlign: "left", fontSize: "300%", fontWeight: "bold", color: "#5554A4", fontFamily: "Righteous"}}>Setup</Col>
-              </Row>
-              <Row>
-                  <Col style={{textAlign: "left", fontSize: "100%", fontWeight: "normal", color: "#000000", fontFamily: "Righteous"}}>Sensor Type</Col>
-                  <Col style={{textAlign: "left", fontSize: "100%", fontWeight: "normal", color: "#000000", fontFamily: "Righteous"}}>ID#/Name</Col>
-                  <Col style={{textAlign: "left", fontSize: "100%", fontWeight: "normal", color: "#000000", fontFamily: "Righteous"}}>Location/Status</Col>
-                  <Col style={{textAlign: "left", fontSize: "100%", fontWeight: "normal", color: "#000000", fontFamily: "Righteous"}}>Other Details</Col>
-              </Row>
-              <div className="spacer" > &nbsp; </div>
-              <div className="spacer" > &nbsp; </div>
-              <div className="spacer" > &nbsp; </div>
+    return (   
+    <div>
+        <Container>
+        <div class = "rectangle">
+            <Row>
+                <Col style={
+                    {position: "absolute",
+                    width: "541px",
+                    height: "105px",
+                    left: "375px",
 
-              <table>
-                  <tr>
-                      <th className={{fontSize:"300%"}}>Sensor Type</th>
-                      <th>ID#/Name</th>
-                      <th>Location/Status</th>
-                      <th>Other Details</th>
-                      
-                  </tr>
-              </table>
+                    fontSize: "300%",
+                    fontWeight: "bold",
+                    color: "#5554A4",
+                    fontFamily: "Righteous"}
+                }>Sensor Setup</Col>
+            </Row>
+              
+            <div className="spacer" > &nbsp; </div>
+            <div className="spacer" > &nbsp; </div>
+            <div className="spacer" > &nbsp; </div>
 
 
-              {/* <Row>
-                  <Col>According to all known laws of aviation, there is no way a bee should be able to fly.</Col>
-                  <Col>Its wings are too small to get its fat little body off the ground.</Col>
-                  <Col>The bee, of course, flies anyway because bees don't care what humans think is impossible.</Col>
-                  <Col>Yellow, black. Yellow, black. Yellow, black. Yellow, black.</Col>
-              </Row>
+                <table border = "1" cellpadding = "10" width = "1000">
+                    <tr>
+                        <th className={{fontSize:"300%"}}>Sensor Type</th>
+                        <th>ID#/Name</th>
+                        <th>Location/Status</th>
+                        <th>Other Details</th>
+                    </tr>
 
-              <Row>
-                    <a href = "https://www.youtube.com/watch?v=L_vnEHDjfZ8">CLICK ME--NOT A RICK ROLL!!</a>
-              </Row> */}
-            </Container>
-        </div>
-        );
+                    <tr>
+                        <td>blank1</td>
+                        <td>blank2</td>
+                        <td>blank3</td>
+                        <td>blank4</td>
+                    </tr>
+                </table>
+            </div>
+
+            <div className="spacer" > &nbsp; </div>
+            <div className="spacer" > &nbsp; </div>
+            <div className="spacer" > &nbsp; </div>
+
+            <a href = "https://www.tutorialspoint.com/html/html_forms.htm">HTML Form stuff (text input)</a>
+            
+            
+        </Container>
+    </div>
+    );
 };
 
 export default Setup;
+
+/*
+.rectangle {
+  width: 200px;
+  height: 100px;
+  background: #EDE4D5;
+}
+
+.magnifying-glass {
+  font-size: 10em;
+  display: inline-block;
+  width: 0.4em;
+  box-sizing: content-box;
+  height: 0.4em;
+  border: 0.1em solid black;
+  position: relative;
+  border-radius: 0.35em;
+}
+.magnifying-glass:before {
+  content: "";
+  display: inline-block;
+  position: absolute;
+  right: -0.25em;
+  bottom: -0.1em;
+  border-width: 0;
+  background: black;
+  width: 0.35em;
+  height: 0.08em;
+  transform: rotate(45deg);
+}
+*/
