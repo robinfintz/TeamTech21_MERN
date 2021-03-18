d14a82bf96682eb40c297920c3cd678c4b4bcc2c
Rajvi <50464351+qv-r@users.noreply.github.com>
2021-03-18 19:28:21 -0400

    Mongo addition

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,33 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+
+
 const express = require('express');
 const testRouter = require('./routers/test');
 const datainputRouter = require('./routers/datainput');
 
-const PORT = process.env.PORT | 5151;
 
-const app = express();
-app.use(express.json());
 
-app.use('/api/test/', testRouter);
-app.use('/api/datainput/', datainputRouter);
+const cString = 'mongodb+srv://user:oWXrnOjoKaNl3MN6@cluster0.rs0u7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; 
+const client = new MongoClient(cString, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    if (err) {
+        throw err
+    }
+    const shipWrecks = client.db("sample_geospatial").collection("shipwrecks");
 
-app.listen(PORT, function() {
-    console.log(`Server now running on port ${PORT}...`);
+    const PORT = process.env.PORT || 5151;
+
+    const app = express()
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.use('/api/test/', testRouter);
+    app.use('/api/datainput/', datainputRouter);
+
+    app.listen(PORT, function() {
+        console.log(`Server now running on port ${PORT}...`);
+    })
 })
